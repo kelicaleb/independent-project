9e7fabb3de7f0faac8af0f56ed801ca6a20952b3
caleb <kelicaleb7@gmail.com>
2021-09-14 15:54:36 +0300

    ip3

---
+++ main.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+    $("#para1").hide();
+    $('#design').click(function(){
+       $('#para1').toggle();
+   });
+   $("#para2").hide();
+   $('#development').click(function(){
+       $('#para2').toggle();
+   });
+   $("#para3").hide();
+   $('#management').click(function(){
+       $('#para3').toggle();
+       
+   });
+
+  })
+const form = document.querySelector("#caleb");
+
+form.addEventListener("submit", function(x){
+    x.preventDefault();
+    const jsform = new FormData(form);  
+    console.log(jsform.get("email"));
+    console.log(jsform.get("username"));
+    if(jsform.get("username") && jsform.get("email")){
+        alert("Thank you for contacting us ")
+    }
+    else{
+        alert('enter your name or email')
+    }
+})
